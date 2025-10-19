1a48469d6c2071fcfde1b38e8416c26cdc94bf62
Douglas Lima <douglas@gemignani.org>
2025-10-19 20:18:09 +0100

    Configure Astro for root domain deployment
    - Remove base path configuration to deploy to gemignani.github.io root
    - Update site URL to https://gemignani.github.io/ without /blog/ suffix
    - Assets will now be served from root paths (/_astro/, /favicon.svg, etc.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gemignani.github.io/blog/',
-	base: '/blog/',
+	site: 'https://gemignani.github.io/',
 	output: 'static',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
